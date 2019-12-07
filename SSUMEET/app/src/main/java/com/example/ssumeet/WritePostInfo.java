37b73a33ec10d72354bf3afe1f9380f55f6c0db3
LEE DONG KON <dle0129@gmail.com>
2019-12-07 14:55:31 +0900

    update

--- SSUMEET/app/src/main/java/com/example/ssumeet/WritePostInfo.java
+++ SSUMEET/app/src/main/java/com/example/ssumeet/WritePostInfo.java
@@ -1,20 +1,72 @@
 package com.example.ssumeet;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 public class WritePostInfo {
     private String title;
-    private String content;
+    private ArrayList<String> contents;
+    private ArrayList<String> formats;
     private String publisher;
+    private Date createdAt;
+    private String id;
 
-    public WritePostInfo(String title, String content, String publisher) {
+
+    public WritePostInfo(String title, ArrayList<String> contents, ArrayList<String> formats, String publisher, Date createdAt){
         this.title = title;
-        this.content = content;
+        this.contents = contents;
+        this.formats = formats;
         this.publisher = publisher;
+        this.createdAt = createdAt;
     }
 
-    public String getTitle() { return this.title ;}
-    public void setTitle() { this.title = title; }
-    public String getContent() { return this.content ;}
-    public void setContent() { this.content = content; }
-    public String getPublisher() { return this.publisher ;}
-    public void setPublisher() { this.title = publisher; }
+    public Map<String, Object> getPostInfo(){
+        Map<String, Object> docData = new HashMap<>();
+        docData.put("title",title);
+        docData.put("contents",contents);
+        docData.put("formats",formats);
+        docData.put("publisher",publisher);
+        docData.put("createdAt",createdAt);
+        return  docData;
+    }
+
+
+    public String getTitle(){
+        return this.title;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+    public ArrayList<String> getContents(){
+        return this.contents;
+    }
+    public void setContents(ArrayList<String> contents){
+        this.contents = contents;
+    }
+    public ArrayList<String> getFormats(){
+        return this.formats;
+    }
+    public void setFormats(ArrayList<String> formats){
+        this.formats = formats;
+    }
+    public String getPublisher(){
+        return this.publisher;
+    }
+    public void setPublisher(String publisher){
+        this.publisher = publisher;
+    }
+    public Date getCreatedAt(){
+        return this.createdAt;
+    }
+    public void setCreatedAt(Date createdAt){
+        this.createdAt = createdAt;
+    }
+    public String getId(){
+        return this.id;
+    }
+    public void setId(String id){
+        this.id = id;
+    }
 }
