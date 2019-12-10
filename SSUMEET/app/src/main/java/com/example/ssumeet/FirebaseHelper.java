29e2a09b3a3fe8ab07d185120008870fbad23118
LEE DONG KON <dle0129@gmail.com>
2019-12-10 14:49:56 +0900

    update

---
+++ SSUMEET/app/src/main/java/com/example/ssumeet/FirebaseHelper.java
@@ -0,0 +1,88 @@
+package com.example.ssumeet;
+
+import android.app.Activity;
+import android.util.Patterns;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+
+public class FirebaseHelper {
+    private Activity activity;
+    private OnPostListener onPostListener;
+    private int successCount;
+
+    public FirebaseHelper(Activity activity) {
+        this.activity = activity;
+    }
+
+    public void setOnPostListener(OnPostListener onPostListener){
+        this.onPostListener = onPostListener;
+    }
+
+    public void storageDelete(final PostInfo postInfo){
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage.getReference();
+
+        final String id = postInfo.getId();
+        ArrayList<String> contentsList = postInfo.getContents();
+        for (int i = 0; i < contentsList.size(); i++) {
+            String contents = contentsList.get(i);
+            if (isStorageUrl(contents)) {
+                successCount++;
+                StorageReference desertRef = storageRef.child("posts/" + id + "/" + storageUrlToName(contents));
+                desertRef.delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        successCount--;
+                        storeDelete(id, postInfo);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        startToast(activity, "Error");
+                    }
+                });
+            }
+        }
+        storeDelete(id, postInfo);
+    }
+
+    private void storeDelete(final String id, final PostInfo postInfo) {
+        FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
+        if (successCount == 0) {
+            firebaseFirestore.collection("posts").document(id)
+                    .delete()
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            startToast(activity, "게시글을 삭제하였습니다.");
+                            onPostListener.onDelete(postInfo);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            startToast(activity, "게시글을 삭제하지 못하였습니다.");
+                        }
+                    });
+        }
+    }
+    public static void  startToast(Activity activity, String msg) {
+        Toast.makeText(activity, msg, Toast.LENGTH_SHORT).show();
+    }
+    public static boolean isStorageUrl(String url){
+        return Patterns.WEB_URL.matcher(url).matches() && url.contains("https://firebasestorage.googleapis.com/v0/b/sns-project-3e2c2.appspot.com/o/post");
+    }
+    public static String storageUrlToName(String url){
+        return url.split("\\?")[0].split("%2F")[url.split("\\?")[0].split("%2F").length - 1];
+    }
+
+}
---
+++ SSUMEET/app/src/main/java/com/example/ssumeet/OnPostListener.java
@@ -0,0 +1,6 @@
+package com.example.ssumeet;
+
+public interface OnPostListener {
+    void onDelete(PostInfo postInfo);
+    void onModify();
+}
