81a131d4f168d5c87609dc7af761fc436c2523a1
Kim-Won-Ho <kwh6543@hanmail.net>
2019-12-11 21:30:39 +0900

    20152362 김원호

--- SSUMEET/app/src/main/java/com/example/ssumeet/MainPage.java
+++ SSUMEET/app/src/main/java/com/example/ssumeet/MainPage.java
@@ -1,6 +1,10 @@
 package com.example.ssumeet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -9,17 +13,27 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
-
+import com.example.ssumeet.chat.SelectUserActivity;
 import com.example.ssumeet.fragment.ChatRoomFragment;
-import com.example.ssumeet.fragment.ProfilePageFragment;
+import com.example.ssumeet.fragment.ProfileFragment;
 import com.example.ssumeet.fragment.UserListFragment;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
+import com.google.firebase.iid.FirebaseInstanceId;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainPage extends AppCompatActivity {
 
     private MainPage.SectionsPagerAdapter mSectionsPagerAdapter;
 
     private ViewPager mViewPager;
+
+    private FloatingActionButton makeRoomBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,8 +42,11 @@ public class MainPage extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
         mSectionsPagerAdapter = new MainPage.SectionsPagerAdapter(getSupportFragmentManager());
 
+        // Set up the ViewPager with the sections adapter.
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
@@ -37,12 +54,16 @@ public class MainPage extends AppCompatActivity {
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
-
+                if (tab.getPosition()==1) {     // char room
+                    //makeRoomBtn.setVisibility(View.VISIBLE);
+                    makeRoomBtn.show();
+                }
             }
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
-
+                //makeRoomBtn.setVisibility(View.INVISIBLE);
+                makeRoomBtn.hide();
             }
 
             @Override
@@ -54,8 +75,55 @@ public class MainPage extends AppCompatActivity {
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
 
+        sendRegistrationToServer();
+
+
+        makeRoomBtn = findViewById(R.id.makeRoomBtn);
+        //makeRoomBtn.setVisibility(View.INVISIBLE);
+        makeRoomBtn.hide();
+        makeRoomBtn.setOnClickListener( new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), SelectUserActivity.class));
+            }
+        });
     }
 
+    void sendRegistrationToServer() {
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        String token = FirebaseInstanceId.getInstance().getToken();
+        Map<String, Object> map = new HashMap<>();
+        map.put("token", token);
+        FirebaseFirestore.getInstance().collection("users").document(uid).set(map, SetOptions.merge());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_logout) {
+            FirebaseAuth.getInstance().signOut();
+            Intent intent = new Intent(this, LoginPage.class);
+            this.startActivity(intent);
+            this.finish();
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
@@ -72,7 +140,7 @@ public class MainPage extends AppCompatActivity {
             switch (position) {
                 case 0: return new UserListFragment();
                 case 1: return new ChatRoomFragment();
-                default: return new ProfilePageFragment();
+                default: return new ProfileFragment();//UserFragment();
             }
         }
 
