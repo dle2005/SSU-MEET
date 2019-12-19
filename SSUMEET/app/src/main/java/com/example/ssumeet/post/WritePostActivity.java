d3556d80f746eaf87469c21f933175b5d04c3cd1
LEE DONG KON <dle0129@gmail.com>
2019-12-20 05:53:43 +0900

    update

--- SSUMEET/app/src/main/java/com/example/ssumeet/post/WritePostActivity.java
+++ SSUMEET/app/src/main/java/com/example/ssumeet/post/WritePostActivity.java
@@ -14,16 +14,14 @@ import android.widget.RelativeLayout;
 import androidx.annotation.NonNull;
 
 import com.bumptech.glide.Glide;
-import com.example.ssumeet.R;
 import com.example.ssumeet.ContentsItemView;
 import com.example.ssumeet.GalleryActivity;
-import com.example.ssumeet.model.ProfileModel;
+import com.example.ssumeet.R;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageMetadata;
@@ -61,9 +59,6 @@ public class WritePostActivity extends BasicActivity {
     private EditText titleEditText;
     private PostInfo postInfo;
     private int pathCount, successCount;
-
-    private com.example.ssumeet.model.ProfileModel ProfileModel;
-    private String user_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -264,7 +259,6 @@ public class WritePostActivity extends BasicActivity {
                                             if (successCount == 0) {
                                                 PostInfo postInfo = new PostInfo(title, contentsList, formatList, user.getUid(), date);
                                                 storeUpload(documentReference, postInfo);
-
                                             }
                                         }
                                     });
@@ -278,23 +272,12 @@ public class WritePostActivity extends BasicActivity {
                 }
             }
             if (successCount == 0) {
-                String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
-
-                DocumentReference docRef = FirebaseFirestore.getInstance().collection("users").document(uid);
-                docRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                    @Override
-                    public void onSuccess(DocumentSnapshot documentSnapshot) {
-                        ProfileModel = documentSnapshot.toObject(ProfileModel.class);
-                        user_name = ProfileModel.getName();
-                    }
-                });
-                Log.d("publisher", user_name);
-                storeUpload(documentReference, new PostInfo(title, contentsList, formatList, user_name, date));
-            }        } else {
+                storeUpload(documentReference, new PostInfo(title, contentsList, formatList, user.getUid(), date));
+            }
+        } else {
             showToast(WritePostActivity.this, "제목을 입력해주세요.");
         }
     }
-
 
     private void storeUpload(DocumentReference documentReference, final PostInfo postInfo) {
         documentReference.set(postInfo.getPostInfo())
